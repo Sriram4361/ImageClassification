1230a9ca42e53bd02266f2da4d53628c44a597bd
Sriram4361 <gsssns4361@gmail.com>
2024-02-13 01:05:37 -0700

    Instead of file read, object created

--- image-classifier-backend/helpers/helper.py
+++ image-classifier-backend/helpers/helper.py
@@ -1,6 +1,5 @@
 import csv
 def find_results(image_filename, csv_file_path):
-    print(image_filename)
     with open(csv_file_path, newline='') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
